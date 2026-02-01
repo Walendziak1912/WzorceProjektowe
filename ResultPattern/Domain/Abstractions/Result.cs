56f60421a7bf9a883f3c96f3d86457101a925a67
Walendziak1912 <walendziak1912@gmail.com>
2026-02-01 12:21:01 +0100

    #Add ResultPattern project with Result and Email classes

---
+++ ResultPattern/Domain/Abstractions/Result.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResultPattern.Domain.Abstractions
+{
+    public class Result
+    {
+        public bool isSuccess { get; init; }
+        public string? ErrorMessage { get; init; } = string.Empty;
+
+        protected Result(bool isSuccess, string? errorMessage)
+        {
+            this.isSuccess = isSuccess;
+            this.ErrorMessage = errorMessage;
+        }
+
+        public static Result Success()
+        {
+            return new Result(true, null);
+        }
+        public static Result Failure(string errorMessage)
+        {
+            return new Result(false, errorMessage);
+        }
+    }
+}
